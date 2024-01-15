9a7ae3e2b06271c09ebccda37b5796e1df0f86af
muhammad <muhammadaljoufi@gmail.com>
2024-01-15 22:19:08 +0500

    made a header for each section

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import { BellIcon } from "lucide-react";
+import SectionName from "./SectionName";
+
+export default function Header() {
+  const date: String = new Date().toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+  return (
+    <nav className="w-full flex justify-between">
+      <div className="flex flex-col gap-2">
+        <SectionName />
+        <p className="text-grey">{date}</p>
+      </div>
+      <div className="flex gap-4 items-center">
+        <div className="cursor-pointer w-10 h-10 flex items-center justify-center p-1 border-2 rounded-full hover:bg-white hover:text-black">
+          <BellIcon className="h-5 w-5" />
+        </div>
+        <div className="cursor-pointer w-10 h-10 bg-white rounded-full"></div>
+        <div>
+          <p className="font-bold">Muhammad Amjad</p>
+          <p className="text-sm text-grey">Admin Store</p>
+        </div>
+      </div>
+    </nav>
+  );
+}
