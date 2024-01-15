1a965193ca52ae8ccf7fa5ba705849be6281d43d
muhammad <muhammadaljoufi@gmail.com>
2024-01-15 22:19:00 +0500

    made a client component for telling the page name

---
+++ src/components/SectionName.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { usePathname } from "next/navigation";
+
+export default function SectionName() {
+  const pathname = usePathname() == "/" ? "/dashboard" : usePathname();
+  return (
+    <h1 className="text-2xl font-bold">
+      {pathname[1].toUpperCase() + pathname.slice(2)}
+    </h1>
+  );
+}
