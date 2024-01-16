8a5fdb5ce6703977914ef9533dfdde6db27a4d3f
muhammad <muhammadaljoufi@gmail.com>
2024-01-16 15:07:46 +0500

    installed charts and refactored the 4 info cards into one file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,9 @@
-import InfoCard from "@/components/dashboard/InfoCard";
-import {
-  FilesIcon,
-  GalleryVerticalEndIcon,
-  PersonStandingIcon,
-  TruckIcon,
-} from "lucide-react";
-
-type DummyInfo = {
-  info: string;
-  mainNumber: number;
-  percentage: number;
-  weekDetail: number;
-  icon: React.JSX.Element;
-};
-
-const dummyData: DummyInfo[] = [
-  {
-    info: "Total Sales",
-    icon: <FilesIcon />,
-    percentage: 2.53,
-    mainNumber: 113738.21,
-    weekDetail: 14.3,
-  },
-  {
-    info: "Total Orders",
-    icon: <GalleryVerticalEndIcon />,
-    percentage: 2.53,
-    mainNumber: 192,
-    weekDetail: 14.3,
-  },
-  {
-    info: "Visitors",
-    icon: <PersonStandingIcon />,
-    percentage: 3.9,
-    mainNumber: 2812,
-    weekDetail: 5,
-  },
-  {
-    info: "Total Sold Products",
-    icon: <TruckIcon />,
-    percentage: 6.9,
-    mainNumber: 112,
-    weekDetail: 5.7,
-  },
-];
+import Infos from "@/components/dashboard/Infos";
 
 export default function Home() {
   return (
     <section className="grid grid-cols-[auto_1fr]">
-      <div className="grid grid-rows-2 grid-cols-2 gap-6">
-        {dummyData.map(
-          ({ icon, info, percentage, mainNumber, weekDetail }, index) => (
-            <InfoCard
-              key={index}
-              info={info}
-              icon={icon}
-              percentage={percentage}
-              mainNumber={mainNumber}
-              weekDetail={weekDetail}
-            />
-          )
-        )}
-      </div>
+      <Infos />
     </section>
   );
 }
---
+++ src/components/dashboard/Infos.tsx
@@ -0,0 +1,65 @@
+import InfoCard from "./InfoCard";
+import {
+  FilesIcon,
+  GalleryVerticalEndIcon,
+  PersonStandingIcon,
+  TruckIcon,
+} from "lucide-react";
+
+type DummyInfo = {
+  info: string;
+  mainNumber: number;
+  percentage: number;
+  weekDetail: number;
+  icon: React.JSX.Element;
+};
+
+const dummyData: DummyInfo[] = [
+  {
+    info: "Total Sales",
+    icon: <FilesIcon />,
+    percentage: 2.53,
+    mainNumber: 113738.21,
+    weekDetail: 14.3,
+  },
+  {
+    info: "Total Orders",
+    icon: <GalleryVerticalEndIcon />,
+    percentage: 2.53,
+    mainNumber: 192,
+    weekDetail: 14.3,
+  },
+  {
+    info: "Visitors",
+    icon: <PersonStandingIcon />,
+    percentage: 3.9,
+    mainNumber: 2812,
+    weekDetail: 5,
+  },
+  {
+    info: "Total Sold Products",
+    icon: <TruckIcon />,
+    percentage: 6.9,
+    mainNumber: 112,
+    weekDetail: 5.7,
+  },
+];
+
+export default function Infos() {
+  return (
+    <div className="grid grid-rows-2 grid-cols-2 gap-6">
+      {dummyData.map(
+        ({ icon, info, percentage, mainNumber, weekDetail }, index) => (
+          <InfoCard
+            key={index}
+            info={info}
+            icon={icon}
+            percentage={percentage}
+            mainNumber={mainNumber}
+            weekDetail={weekDetail}
+          />
+        )
+      )}
+    </div>
+  );
+}
