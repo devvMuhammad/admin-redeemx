4a07baa512d1a0adf3ae96aaeb980d375fc282f7
muhammad <muhammadaljoufi@gmail.com>
2024-01-16 19:14:14 +0500

    added colors to pie chart

--- src/components/dashboard/PieChart.tsx
+++ src/components/dashboard/PieChart.tsx
@@ -1,16 +1,16 @@
 "use client";
 import { ChartData } from "chart.js";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Pie } from "react-chartjs-2";
+import { Doughnut, Pie } from "react-chartjs-2";
 
-const data: ChartData<"pie", number[], unknown> = {
+const data: ChartData<"doughnut", number[], unknown> = {
   labels: ["Laptop", "Gift Cards", "Mobiles", "Accessories"],
   // consists of array of object, with each object representing a data
   datasets: [
     {
       label: "Quantity Sold",
       data: [20, 10, 3, 2],
-      backgroundColor: ["#fff"],
+      backgroundColor: ["#00607a", "#705831", "#440f3b", "#700c0c"],
       borderColor: "black",
     },
   ],
@@ -20,7 +20,18 @@ export default function PieChart() {
   ChartJS.register(ArcElement, Tooltip, Legend);
   return (
     <div className="flex items-center justify-center">
-      <Pie className="fixed" data={data} />
+      <Doughnut
+        className="fixed"
+        data={data}
+        options={{
+          plugins: {
+            legend: {
+              labels: { color: "white", font: { size: 16 } },
+              position: "top",
+            },
+          },
+        }}
+      />
     </div>
   );
 }
