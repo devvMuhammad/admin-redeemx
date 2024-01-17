041d29a1f8b50b805f0ef7be1a1c99b0640ed963
muhammad <muhammadaljoufi@gmail.com>
2024-01-17 19:55:17 +0500

    made a reusable h1 component

---
+++ src/components/ui/Heading.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils";
+
+export default function Heading({
+  className,
+  children,
+}: {
+  className: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <h1 className={cn("font-extrabold text-4xl tracking-tighter", className)}>
+      {children}
+    </h1>
+  );
+}
