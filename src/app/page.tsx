34fe8d229fb9adc64166a97f2e4ec03848c88aec
muhammad <muhammadaljoufi@gmail.com>
2024-01-17 15:36:05 +0500

    small fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import PieChart from "@/components/dashboard/PieChart";
 export default function Home() {
   return (
     <section className="grid grid-cols-[auto_1fr] overflow-y-auto">
-      <div className="grid-rows-1">
+      <div>
         <Infos />
       </div>
-      <div className="grid-rows-1">
+      <div>
         <PieChart />
       </div>
     </section>
