591452a7acfdbf78cc80bb6ccbcc52b0c4eab218
muhammad <muhammadaljoufi@gmail.com>
2024-01-18 22:12:01 +0500

    added a products table

--- src/components/inventory/ProductsTable.tsx
+++ src/components/inventory/ProductsTable.tsx
@@ -10,7 +10,6 @@ type Product = {
   status: "Active" | "Out of Stock";
   revenue: number;
 };
-
 const dummyProducts: Product[] = [
   {
     name: 'TechMaster EliteBook X360 G8 UltraSlim Intel Core i7 16GB RAM 512GB SSD 14" FHD Touchscreen Windows 11 Pro Laptop',
@@ -36,19 +35,64 @@ const dummyProducts: Product[] = [
     status: "Active",
     revenue: 0,
   },
+  // Additional products
+  {
+    name: 'SuperTech UltraThin Core i5 8GB RAM 256GB SSD 13.3" FHD Windows 11 Laptop',
+    category: "Laptops",
+    id: "STUCI5",
+    price: 1099.99,
+    status: "Active",
+    revenue: 0,
+  },
+  {
+    name: "GigaSpeed Gaming PC Ryzen 7 16GB RAM 1TB HDD + 512GB SSD RTX 3060 Windows 11 Desktop",
+    category: "Desktops",
+    id: "GGPR716",
+    price: 1899.99,
+    status: "Active",
+    revenue: 0,
+  },
+  {
+    name: 'SmartOffice Pro All-in-One Intel Core i5 8GB RAM 512GB SSD 23.8" FHD Windows 11 Desktop',
+    category: "Desktops",
+    id: "SOPAI5",
+    price: 1299.99,
+    status: "Active",
+    revenue: 0,
+  },
+  {
+    name: 'EcoFriendly Notebook AMD Ryzen 5 12GB RAM 256GB SSD 14" HD Windows 11 Laptop',
+    category: "Laptops",
+    id: "EFNAR5",
+    price: 899.99,
+    status: "Active",
+    revenue: 0,
+  },
+  {
+    name: 'MaxPower Workstation Intel Xeon 128GB ECC RAM 4TB PCIe Gen4 SSD 27" 5K HDR Windows 11 Pro Desktop',
+    category: "Desktops",
+    id: "MPWIX",
+    price: 4999.99,
+    status: "Active",
+    revenue: 0,
+  },
+  {
+    name: 'MediaMaster Plus Ryzen 7 24GB RAM 512GB SSD 17.3" FHD Windows 11 Laptop',
+    category: "Laptops",
+    id: "MMPR724",
+    price: 1599.99,
+    status: "Active",
+    revenue: 0,
+  },
 ];
 
 const inter = Inter({ weight: "400", style: "normal" });
 
 export default function ProductsTable() {
-  // <div className="bg-yellow-500 grid grid-cols-[auto_1fr_1fr_1fr_1fr_1fr_1fr] justify-center items-center text-center ">
   return (
-    // <div className="bg-yellow-500 grid">
-    // div
     <div className="overflow-x-auto">
       <div className="min-w-[700px] header product-rows grid grid-rows-4 grid-cols-[auto_3fr_1fr_1fr_1fr_1fr_1fr] justify-center items-center text-center overflow-x-auto ">
         <TableHeader />
-        {/* </div> */}
         {dummyProducts.map((product, i) => (
           <>
             <p className={`${inter.className}`}>{i + 1}</p>
@@ -65,13 +109,10 @@ export default function ProductsTable() {
                 {product.status}
               </Badge>
             </div>
-            {/* </p> */}
             <p className={`${inter.className}`}>{product.revenue.toFixed(2)}</p>
           </>
-          // </div>
         ))}
       </div>
     </div>
-    // </div>
   );
 }
