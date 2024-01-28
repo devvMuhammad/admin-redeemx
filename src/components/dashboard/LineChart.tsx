79ea2e928302a1abadc4664356a4a137a95e4edf
muhammad <muhammadaljoufi@gmail.com>
2024-01-29 00:16:43 +0500

    set responsive to true

--- src/components/dashboard/LineChart.tsx
+++ src/components/dashboard/LineChart.tsx
@@ -61,6 +61,7 @@ export default function LineChart() {
         }}
         options={{
           animation: false,
+          responsive: true,
           scales: {
             x: {
               title: {
