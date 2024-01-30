e89549cbf5277b36db7dd5977ffe645853d21c5b
muhammad <muhammadaljoufi@gmail.com>
2024-01-30 13:10:23 +0500

    a utility function for camel to title case

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function toTitleCase(input: string) {
+  // Replace camel case with spaces and capitalize each word
+  return input
+    .replace(/([a-z])([A-Z])/g, "$1 $2")
+    .replace(/\b\w/g, (firstLetter) => firstLetter.toUpperCase());
 }
