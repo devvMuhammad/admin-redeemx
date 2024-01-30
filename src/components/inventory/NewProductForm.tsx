756feb1f2e14698b7b1c8c0c9fd99eb59f633224
muhammad <muhammadaljoufi@gmail.com>
2024-01-30 13:10:57 +0500

    completed the form for adding new product

--- src/components/inventory/NewProductForm.tsx
+++ src/components/inventory/NewProductForm.tsx
@@ -10,14 +10,21 @@ import { zodResolver } from "@hookform/resolvers/zod";
 const formSchema = z.object({
   name: z.string().min(1, { message: "Product Name is required" }),
   category: z.string().min(1, { message: "Category is required" }),
-  price: z.number().min(1, { message: "Price is required" }),
+  price: z
+    .number()
+    .min(1, { message: "Price is required" })
+    .positive("Price must be positive"),
   stock: z.number().min(1, { message: "Initial stock is required" }),
 });
 
 type FormSchema = z.infer<typeof formSchema>;
 
 export default function NewProductForm() {
-  const { handleSubmit } = useForm<FormSchema>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
   });
   function submitHandler(data: FormSchema) {
@@ -28,18 +35,38 @@ export default function NewProductForm() {
       onSubmit={handleSubmit(submitHandler)}
       className="flex flex-col gap-6"
     >
-      <div className="grid items-center grid-cols-[auto_1fr] gap-4">
-        <Label htmlFor="name">Name</Label>
-        <Input id="name" />
-        <p className="text-red-500"></p>
-        <Label htmlFor="category">Category</Label>
-        <CategorySelect />
-
-        <Label htmlFor="price">Price</Label>
-        <Input id="price" type="number" className="w-1/2" />
-
-        <Label htmlFor="stock">Set Stock</Label>
-        <Input id="stock" type="number" className="w-1/2" />
+      <div className="flex flex-col gap-2">
+        <div className="grid items-center grid-cols-[1fr_3fr] gap-x-2">
+          <Label htmlFor="name">Name</Label>
+          <Input id="name" {...register("name")} />
+        </div>
+        {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+        <div className="grid items-center grid-cols-[1fr_3fr] gap-x-2">
+          <Label htmlFor="category">Category</Label>
+          <CategorySelect />
+        </div>
+        <div className="grid items-center grid-cols-[1fr_3fr] gap-x-2">
+          <Label htmlFor="price">Price</Label>
+          <Input
+            id="price"
+            defaultValue={0}
+            type="number"
+            className="w-1/2"
+            {...register("price", { valueAsNumber: true })}
+          />
+        </div>
+        {errors.price && <p className="text-red-500">{errors.price.message}</p>}
+        <div className="grid items-center grid-cols-[1fr_3fr] gap-x-2">
+          <Label htmlFor="stock">Set Stock</Label>
+          <Input
+            id="stock"
+            type="number"
+            defaultValue={0}
+            className="w-1/2"
+            {...register("stock", { valueAsNumber: true })}
+          />
+        </div>
+        {errors.stock && <p className="text-red-500">{errors.stock.message}</p>}
       </div>
       <Button className="self-end font-bold">Add Product</Button>
     </form>
