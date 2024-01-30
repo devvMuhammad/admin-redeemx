332b120faffae78f3b143042421e0e1bbbe8157c
muhammad <muhammadaljoufi@gmail.com>
2024-01-30 23:39:50 +0500

    made a connection file

---
+++ src/db/connect.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+// maintaining a cached connection to prevent reconnection
+let cached = (global as any).mongoose;
+// reset the connection if it doesnt exist
+if (!cached) {
+  cached = (global as any).mongoose = null;
+}
+
+export async function connectDB() {
+  // return the cache if it exists
+  if (cached) {
+    return cached;
+  }
+  // make the connection and cache it
+  const requestURL = `mongodb+srv://muhammadaljoufi:${process.env.DB_PASSWORD}@cluster0.fhnovob.mongodb.net/app`;
+  cached = await mongoose.connect(requestURL);
+  if (cached) console.log("A connection was established to the databse");
+  return cached;
+}
