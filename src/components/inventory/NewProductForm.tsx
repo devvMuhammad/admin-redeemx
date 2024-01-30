f20acac06f552569630fa121ca53ea44d24e08ed
devvMuhammad <muhammadaljoufi@gmail.com>
2024-01-30 18:38:53 +0500

    made a small change in price input field

--- src/components/inventory/NewProductForm.tsx
+++ src/components/inventory/NewProductForm.tsx
@@ -12,8 +12,8 @@ const formSchema = z.object({
   category: z.string().min(1, { message: "Category is required" }),
   price: z
     .number()
-    .min(1, { message: "Price is required" })
-    .positive("Price must be positive"),
+    .positive("Price must be positive")
+    .min(1, { message: "Price is required" }),
   stock: z.number().min(1, { message: "Initial stock is required" }),
 });
 
