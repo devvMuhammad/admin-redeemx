a6e8399d2368e9aaa75b6b8a7c9f5bac2abf0e73
devvMuhammad <muhammadaljoufi@gmail.com>
2024-02-03 20:50:34 +0500

    chnaged font styling

--- src/components/inventory/ProductControls.tsx
+++ src/components/inventory/ProductControls.tsx
@@ -1,8 +1,11 @@
+import { Inter } from "next/font/google";
 import CategorySelect from "../ui/CategorySelect";
 import SearchInput from "../ui/SearchInput";
 import SortSelect from "../ui/SortSelect";
 import { Button } from "../ui/button";
 import NewProduct from "./NewProduct";
+
+const inter = Inter({ weight: ["400", "600"], style: "normal" });
 
 export default function ProductControls() {
   /*
@@ -23,7 +26,10 @@ export default function ProductControls() {
         {/* sorting options */}
         <SortSelect />
 
-        <Button size="lg" className="font-bold text-lg p-4">
+        <Button
+          size="lg"
+          className={` ${inter.className} font-bold text-lg p-4`}
+        >
           Export as CV
         </Button>
         <NewProduct />
