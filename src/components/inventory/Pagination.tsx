b8179925993911a9083aed7150a9304f3fbb724f
devvMuhammad <muhammadaljoufi@gmail.com>
2024-02-18 01:36:44 +0500

    added pagination component

--- src/components/inventory/Pagination.tsx
+++ src/components/inventory/Pagination.tsx
@@ -1,3 +1,10 @@
 export default function Pagination() {
-  return <h1>Pagination...</h1>;
+  return (
+    <div className="flex justify-between border border-zinc-700 p-2 text-lg rounded-lg">
+      <h1>
+        Showing <span className="font-bold">10</span> of{" "}
+        <span className="font-bold">15</span> Products
+      </h1>
+    </div>
+  );
 }
