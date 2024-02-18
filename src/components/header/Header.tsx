3b8d7cd84da50331699ced046872f6b666d0cde1
devvMuhammad <muhammadaljoufi@gmail.com>
2024-02-18 11:55:45 +0500

    small changes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
     year: "numeric",
   });
   return (
-    <nav className="flex justify-between mt-3 border-b border-b-grey p-3">
+    <nav className="flex justify-between mt-3 border-b border-b-grey px-3 py-1">
       <div className="flex flex-col gap-2">
         <SectionName />
         <p className="text-sm md:text-lg text-grey">{date}</p>
---
+++ src/components/inventory/ProductsHeader.tsx
@@ -0,0 +1,14 @@
+export default function ProductsHeader() {
+  return (
+    <>
+      <span className="text-left">No.</span>
+      <span>Product Name</span>
+      <span>Category</span>
+      <span>ID</span>
+      <span>Price</span>
+      <span>Status</span>
+      <span>Revenue</span>
+      <span>&nbsp;</span>
+    </>
+  );
+}
