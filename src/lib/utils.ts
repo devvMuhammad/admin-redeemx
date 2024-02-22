13a4788252c406310ae8e7ff211f7829d872b380
devvMuhammad <muhammadaljoufi@gmail.com>
2024-02-23 02:48:12 +0500

    small helper function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function toTitleCase(input: string) {
+export function toDashCase(input: string): string {
+  return input.toLowerCase().split(" ").join("-");
+}
+
+export function toTitleCase(input: string): string {
   // Replace camel case with spaces and capitalize each word
   return input
     .replace(/([a-z])([A-Z])/g, "$1 $2")
