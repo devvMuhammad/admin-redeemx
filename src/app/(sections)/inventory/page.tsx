a02d36ce6d7ea4d663a050e962ff745b2d194bac
devvMuhammad <muhammadaljoufi@gmail.com>
2024-02-29 01:19:17 +0500

    passed props to the getProducts function

--- src/app/(sections)/inventory/page.tsx
+++ src/app/(sections)/inventory/page.tsx
@@ -1,18 +1,30 @@
 import ProductControls from "@/components/inventory/ProductControls";
 import ProductsTable from "@/components/inventory/ProductsTable";
-import { prisma } from "../../../../prisma/client";
 
-export default async function Inventory() {
-  const products = await prisma.products.findMany({
-    orderBy: {
-      name: "asc",
-    },
+import { getProducts } from "@/lib/getProducts";
+
+type searchParams = {
+  name: string;
+  search: string;
+  category: string;
+  price: string;
+  page: number;
+};
+
+export default async function Inventory({
+  searchParams: { name, search, category, price, page },
+}: {
+  searchParams: searchParams;
+}) {
+  const { products, numberOfProducts } = await getProducts({
+    category,
+    page,
   });
-  console.log(products);
+  console.log({ products, numberOfProducts });
   return (
     <section className="space-y-4">
       <ProductControls />
-      <ProductsTable products={products} />
+      <ProductsTable products={products} numberOfProducts={numberOfProducts} />
     </section>
   );
 }
