90c216107c7e546f12882ae3db5ea1547f731a0a
devvMuhammad <muhammadaljoufi@gmail.com>
2024-02-29 13:25:01 +0500

    adding suspense boundary

--- src/app/(sections)/inventory/page.tsx
+++ src/app/(sections)/inventory/page.tsx
@@ -1,7 +1,9 @@
 import ProductControls from "@/components/inventory/ProductControls";
 import ProductsTable from "@/components/inventory/ProductsTable";
+import Heading from "@/components/ui/Heading";
 
 import { getProducts } from "@/lib/getProducts";
+import { Suspense } from "react";
 
 type searchParams = {
   name: string;
@@ -24,7 +26,19 @@ export default async function Inventory({
   return (
     <section className="space-y-4">
       <ProductControls />
-      <ProductsTable products={products} numberOfProducts={numberOfProducts} />
+      <Suspense fallback={<h1>Loading products...</h1>}>
+        {numberOfProducts > 0 ? (
+          <ProductsTable
+            products={products}
+            numberOfProducts={numberOfProducts}
+          />
+        ) : (
+          <div className="text-center">
+            <Heading>No such products!</Heading>
+            <p>No products for such filters. Kindly change your filters</p>
+          </div>
+        )}
+      </Suspense>
     </section>
   );
 }
