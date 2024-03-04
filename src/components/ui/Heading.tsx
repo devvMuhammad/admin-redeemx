dcde0ccb897f42b475dda94757d18615737c38bd
devvMuhammad <muhammadaljoufi@gmail.com>
2024-03-05 00:08:34 +0500

    added montserrat font for special headings

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -1,14 +1,29 @@
 import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const poopins = Poppins({
+  weight: ["400", "600"],
+  style: "normal",
+  subsets: ["latin"],
+});
 
 export default function Heading({
   className,
   children,
+  specialHeading = true,
 }: {
   className?: string;
   children: React.ReactNode;
+  specialHeading?: boolean;
 }) {
   return (
-    <h1 className={cn("font-extrabold text-4xl tracking-tighter", className)}>
+    <h1
+      className={cn(
+        "font-extrabold text-4xl tracking-tighter",
+        specialHeading && poopins.className,
+        className
+      )}
+    >
       {children}
     </h1>
   );
