f332509aba233a8bb5c6a5073b8f90427b0c428e
devvMuhammad <muhammadaljoufi@gmail.com>
2024-03-05 01:18:50 +0500

    used heading compoent inplace of h1

--- src/components/header/SectionName.tsx
+++ src/components/header/SectionName.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { usePathname } from "next/navigation";
+import Heading from "../ui/Heading";
 
 export default function SectionName() {
   const pathname = usePathname() == "/" ? "/dashboard" : usePathname();
   return (
-    <h1 className="text-lg md:text-2xl font-bold">
+    <Heading className="text-lg md:text-2xl font-bold">
       {pathname[1].toUpperCase() + pathname.slice(2)}
-    </h1>
+    </Heading>
   );
 }
