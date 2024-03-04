e7bd1455cc2e29bb4e7f3fda7e61c6f4d447d83c
devvMuhammad <muhammadaljoufi@gmail.com>
2024-03-05 01:19:18 +0500

    started working on the settings page

--- src/app/(sections)/settings/page.tsx
+++ src/app/(sections)/settings/page.tsx
@@ -1,3 +1,14 @@
+import AppearanceForm from "@/components/orders/AppearanceForm";
+import ProfileForm from "@/components/orders/ProfileForm";
+import HrLine from "@/components/ui/hr";
+
 export default function Settings() {
-  return <p>Settings</p>;
+  return (
+    <>
+      <ProfileForm />
+      <HrLine className="my-10" />
+      <AppearanceForm />
+      <HrLine className="my-10" />
+    </>
+  );
 }
