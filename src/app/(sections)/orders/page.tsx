a764ae75f15facdfa024936050a1bb9a278df686
devvMuhammad <muhammadaljoufi@gmail.com>
2024-03-09 00:33:09 +0500

    added fallback incase of zero errors

--- src/app/(sections)/orders/page.tsx
+++ src/app/(sections)/orders/page.tsx
@@ -2,23 +2,34 @@ import getOrders from "@/actions/getOrders";
 import Pagination from "@/components/inventory/Pagination";
 import OrdersControl from "@/components/orders/OrdersControl";
 import OrdersTable from "@/components/orders/OrdersTable";
+import Heading from "@/components/ui/Heading";
+
 export default async function Orders({
-  searchParams: { page, sort },
+  searchParams: { page, sort, amount },
 }: {
-  searchParams: { page?: string; sort?: string };
+  searchParams: { page: string; sort: string; amount: string };
 }) {
-  const { orders, numberOfOrders } = await getOrders({ page, sort });
+  const { orders, numberOfOrders } = await getOrders({ page, sort, amount });
   console.log(orders);
   return (
     <section className="space-y-4">
-      <OrdersControl />
-      <OrdersTable orders={orders} />
-      {/* WILL ADD THIS LATER TO THE ORDERS TABLE */}
-      <Pagination
-        currentItems={orders.length}
-        numberOfItems={numberOfOrders}
-        perPage={10}
-      />
+      {numberOfOrders > 0 ? (
+        <>
+          {" "}
+          <OrdersControl />
+          <OrdersTable orders={orders} />
+          <Pagination
+            currentItems={orders.length}
+            numberOfItems={numberOfOrders}
+            perPage={10}
+          />{" "}
+        </>
+      ) : (
+        <div className="text-center">
+          <Heading>No such orders!</Heading>
+          <p>No orders for such filters. Kindly change your filters</p>
+        </div>
+      )}
     </section>
   );
 }
