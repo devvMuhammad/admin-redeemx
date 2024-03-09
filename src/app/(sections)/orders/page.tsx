a20b198a67e7fa6eff2db4d33fe75cd8281b6ba0
devvMuhammad <muhammadaljoufi@gmail.com>
2024-03-09 12:26:56 +0500

    customized empty products in case of no products

--- src/app/(sections)/orders/page.tsx
+++ src/app/(sections)/orders/page.tsx
@@ -1,35 +1,42 @@
-import getOrders from "@/actions/getOrders";
+import getOrders, { TOrdersSchema } from "@/actions/getOrders";
 import Pagination from "@/components/inventory/Pagination";
 import OrdersControl from "@/components/orders/OrdersControl";
 import OrdersTable from "@/components/orders/OrdersTable";
 import Heading from "@/components/ui/Heading";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Orders({
-  searchParams: { page, sort, amount },
+  searchParams: { page, sort, amount, search },
 }: {
-  searchParams: { page: string; sort: string; amount: string };
+  searchParams: { page: string; sort: string; amount: string; search: string };
 }) {
-  const { orders, numberOfOrders } = await getOrders({ page, sort, amount });
+  const { orders, numberOfOrders } = await getOrders({
+    page,
+    sort,
+    amount,
+    search,
+  });
   console.log(orders);
   return (
     <section className="space-y-4">
+      <OrdersControl />
       {numberOfOrders > 0 ? (
-        <>
-          {" "}
-          <OrdersControl />
-          <OrdersTable orders={orders} />
-          <Pagination
-            currentItems={orders.length}
-            numberOfItems={numberOfOrders}
-            perPage={10}
-          />{" "}
-        </>
+        <OrdersTable orders={orders} />
       ) : (
-        <div className="text-center">
+        <div className="text-center space-y-2">
           <Heading>No such orders!</Heading>
           <p>No orders for such filters. Kindly change your filters</p>
+          <Link href="/orders" className="inline-block">
+            <Button>Reset Filters</Button>
+          </Link>
         </div>
       )}
+      <Pagination
+        currentItems={orders.length}
+        numberOfItems={numberOfOrders}
+        perPage={10}
+      />{" "}
     </section>
   );
 }
