80b19e4477cd6c1990bb237b841174e8375b2234
devvMuhammad <muhammadaljoufi@gmail.com>
2024-03-10 12:32:48 +0500

    small fix with the pathname hook

--- src/components/header/SectionName.tsx
+++ src/components/header/SectionName.tsx
@@ -3,10 +3,11 @@ import { usePathname } from "next/navigation";
 import Heading from "../ui/Heading";
 
 export default function SectionName() {
-  const pathname = usePathname() == "/" ? "/dashboard" : usePathname();
+  const pathname = usePathname();
+  const name = pathname == "/" ? "/dashboard" : pathname;
   return (
     <Heading className="text-lg md:text-2xl font-bold">
-      {pathname[1].toUpperCase() + pathname.slice(2)}
+      {name[1].toUpperCase() + name.slice(2)}
     </Heading>
   );
 }
