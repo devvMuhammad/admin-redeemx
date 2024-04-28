d7e9546c2219c8f4ae10bae8612925c9038f9171
muhammad <muhammadaljoufi@gmail.com>
2024-04-28 22:24:33 +0500

    css fix

--- src/app/(sections)/page.tsx
+++ src/app/(sections)/page.tsx
@@ -6,9 +6,7 @@ import TopProducts from "@/components/dashboard/TopProduct";
 export default function Home() {
   return (
     <section className="grid grid-cols-1 gap-4 lg:grid-cols-[auto_1fr]">
-      <div>
-        <Infos />
-      </div>
+      <Infos />
       <div className="border rounded-xl border-gray-700">
         <PieChart />
       </div>
